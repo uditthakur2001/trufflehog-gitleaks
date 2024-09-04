950327d61db24c511d03aed84fd2a41be66f0366
= <=>
2024-09-04 18:42:43 +0530

    first

---
+++ secrets.py
@@ -0,0 +1,17 @@
+secrets = { 
+	'SECRET_KEY': "1tUm636uS1yOEcfP5pvfqJ/ml36mF7AkyHsEU0I", 
+	'DATABASE_USER': "testusr", 
+	'DATABASE_PASSWORD': 'pass1234', 
+	'DATABASE_PORT': 5432,
+    "aws":{
+        "access_key_id": "AKIAIOSFODNN7EXAMPLE",
+  		"secret_access_key":"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+	},
+    "azure":{
+        
+  	"storage_account_name": "examplestorageaccount",
+  	"storage_account_key": "Eby8vdM02xNOcqFlq1XN2v==YOUR_EXAMPLE_KEY==jaNjwGDW==EXAMPLEKEY=="
+	}
+
+} 
+ # type: ignore
